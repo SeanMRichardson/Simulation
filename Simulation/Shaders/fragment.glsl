4c83f5f8dfffb7a13b34c8ab757d7b324dfd6f46
SeanMRichardson <Sean.r2000@hotmail.com>
2017-07-20 15:12:16 +0100

    ambient, diffuse, and specular lighting added working fine

--- Simulation/Shaders/fragment.glsl
+++ Simulation/Shaders/fragment.glsl
@@ -2,7 +2,32 @@
 
 out vec4 FragColour;
 
+uniform vec3 lightColour;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+in vec3 FragPos;
+in vec3 Normal;
+
 void main()
 {
-	FragColour = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+	float ambientStrength = 0.1f;
+	vec3 ambient = ambientStrength * lightColour;
+
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(lightPos - FragPos);
+
+	float diff = max(dot(norm, lightDir), 0.0f);
+	vec3 diffuse = diff * lightColour;
+
+	float specularStrength = 0.5f;
+	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 reflectDir = reflect(-lightDir, norm);
+
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+	vec3 specular = specularStrength * spec * lightColour;
+
+	vec3 result = (ambient + diffuse + specular) * vec3(0.5f, 0.2f, 0.5f);
+
+	FragColour =  vec4(result,1.0);
 }
--- Simulation/Simulation.cpp
+++ Simulation/Simulation.cpp
@@ -23,9 +23,11 @@ void scrollCallback(GLFWwindow* window, double xOffset, double yOffest);
 
 const GLint WIDTH = 800, HEIGHT = 600;
 
-Shader* shader;
+Shader* shader, *lampShader;
 
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+glm::vec3 cameraPos = glm::vec3(0.5f, 0.75f, 5.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
@@ -75,10 +77,12 @@ int main()
 
 	// attach window
 	glfwMakeContextCurrent(window);
-	glfwSetCursorPosCallback(window, mouseCallback);
-	glfwSetScrollCallback(window, scrollCallback);
 
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	//// camera bindings
+	//glfwSetCursorPosCallback(window, mouseCallback);
+	//glfwSetScrollCallback(window, scrollCallback);
+
+	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	// use modern approach to retrieve function pointers etc.
 	glewExperimental = GL_TRUE;
@@ -142,7 +146,7 @@ int main()
 
 	glm::vec3 cubePositions[] = {
 		glm::vec3(0.0f,  0.0f,  0.0f),
-		glm::vec3(2.0f,  5.0f, -15.0f),
+		/*glm::vec3(2.0f,  5.0f, -15.0f),
 		glm::vec3(-1.5f, -2.2f, -2.5f),
 		glm::vec3(-3.8f, -2.0f, -12.3f),
 		glm::vec3(2.4f, -0.4f, -3.5f),
@@ -150,7 +154,7 @@ int main()
 		glm::vec3(1.3f, -2.0f, -2.5f),
 		glm::vec3(1.5f,  2.0f, -2.5f),
 		glm::vec3(1.5f,  0.2f, -1.5f),
-		glm::vec3(-1.3f,  1.0f, -1.5f)
+		glm::vec3(-1.3f,  1.0f, -1.5f)*/
 	};
 
 	GLuint indices[] =
@@ -167,18 +171,18 @@ int main()
 		1.0f, 1.0f
 	};
 
+	glEnable(GL_DEPTH_TEST);
+
 	shader = new Shader("Shaders/vertex.glsl", "Shaders/fragment.glsl");
 	if (!shader->LinkProgram())
 	{
 		return 0;
 	}
-
-	
-
-	// create a vao
-	GLuint vao;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
+	lampShader = new Shader("Shaders/lampVertex.glsl", "Shaders/lampFragment.glsl");
+	if (!lampShader->LinkProgram())
+	{
+		return 0;
+	}
 
 	// create vbo
 	GLuint vbo;
@@ -193,6 +197,22 @@ int main()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+	// create a vao
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(1);
+
+	GLuint lightVao;
+	glGenVertexArrays(1, &lightVao);
+	glBindVertexArray(lightVao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
 	glEnableVertexAttribArray(0);
 
@@ -201,30 +221,6 @@ int main()
 	GLint numAttributes;
 	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &numAttributes);
 	std::cout << "Maximum number of vertex attributes supported: " << numAttributes << std::endl;
-
-	/*GLuint texture;
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	GLint width, height, numChannels;
-	unsigned char* data = ;*/
-
-	//glm::mat4 trans;
-	//trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
-	//trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-	////trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));
-
-	//GLuint transformLoc = glGetUniformLocation(shader->GetProgram(), "transform");
-	//glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-	glUseProgram(shader->GetProgram());
-	
-	
-	
 
 	// simulation loop
 	while (!glfwWindowShouldClose(window))
@@ -238,8 +234,8 @@ int main()
 		processInput(window);
 
 		// reset the color
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		float radius = 10.0f;
 		float camX = sin(glfwGetTime()) * radius;
@@ -249,32 +245,45 @@ int main()
 		view = glm::lookAt(	cameraPos, cameraPos + cameraFront, cameraUp);
 
 		glm::mat4 proj;
-		proj = glm::perspective(glm::radians(fov), (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
+		proj = glm::perspective(glm::radians(45.0f), (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
 
 		// draw stuff
 		glUseProgram(shader->GetProgram());
 
-		/*glm::mat4 model;
-		model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));*/
-		
+		glUniform3f(glGetUniformLocation(shader->GetProgram(), "lightColour"), 1.0f, 1.0f, 1.0f);
+		glUniform3f(glGetUniformLocation(shader->GetProgram(), "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+		glUniform3f(glGetUniformLocation(shader->GetProgram(), "viewPos"), cameraPos.x, cameraPos.y, cameraPos.z);
+
 		glUniformMatrix4fv(glGetUniformLocation(shader->GetProgram(), "view"), 1, GL_FALSE, glm::value_ptr(view));
 		glUniformMatrix4fv(glGetUniformLocation(shader->GetProgram(), "proj"), 1, GL_FALSE, glm::value_ptr(proj));
 
 		glBindVertexArray(vao);	
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		//glBindVertexArray(0);
-
-		//for (unsigned int i = 0; i < 10; i++)
-		//{
+		glm::mat4 model;
+		for (unsigned int i = 0; i < 10; i++)
+		{
 			// calculate the model matrix for each object and pass it to shader before drawing
-			glm::mat4 model;
-			model = glm::translate(model, cubePositions[0]);
-			float angle = 20.0f * 1;
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
 			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 			glUniformMatrix4fv(glGetUniformLocation(shader->GetProgram(), "model"), 1, GL_FALSE, glm::value_ptr(model));
 
 			glDrawArrays(GL_TRIANGLES, 0, 36);
-		//}
+		}
+		glUseProgram(lampShader->GetProgram());
+
+		glUniformMatrix4fv(glGetUniformLocation(lampShader->GetProgram(), "view"), 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(glGetUniformLocation(lampShader->GetProgram(), "proj"), 1, GL_FALSE, glm::value_ptr(proj));
+
+		model = glm::mat4();
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.2f));
+
+		glUniformMatrix4fv(glGetUniformLocation(lampShader->GetProgram(), "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+		glBindVertexArray(lightVao);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		// write to buffer
 		glfwSwapBuffers(window);
@@ -305,6 +314,7 @@ void processInput(GLFWwindow* window)
 		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+
 }
 
 void mouseCallback(GLFWwindow* window, double xPos, double yPos)
